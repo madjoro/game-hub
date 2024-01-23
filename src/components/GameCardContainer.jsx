5990029a18adc57bf943b47e7806a9567fdb430e
madjo7 <markodjo7@gmail.com>
2024-01-23 17:48:18 +0100

    Display genres

--- src/components/GameCardContainer.jsx
+++ src/components/GameCardContainer.jsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 const GameCardContainer = ({ children }) => {
   return (
-    <Box borderRadius={10} overflow="hidden" width={"300px"}>
+    <Box borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,14 +1,24 @@
-import useData from "../hooks/useData";
 import useGenres from "../hooks/useGenres";
+import { List, ListItem, HStack, Image, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((g) => (
-        <li key={g.id}>{g.name}</li>
+        <ListItem key={g.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize={"32px"}
+              borderRadius={8}
+              src={getCroppedImageUrl(g.image_background)}
+            ></Image>
+            <Text fontSize={"lg"}>{g.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
