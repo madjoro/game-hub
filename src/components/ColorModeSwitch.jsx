0501d2b6f6084b029b81c1eee96ebe41f822e88d
madjo <78071957+madjo7@users.noreply.github.com>
2024-01-29 11:31:50 +0100

    Build search input

--- src/components/ColorModeSwitch.jsx
+++ src/components/ColorModeSwitch.jsx
@@ -9,7 +9,7 @@ const ColorModeSwitch = () => {
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       ></Switch>
-      <Text></Text>
+      <Text whiteSpace={"nowrap"}>Dark Mode</Text>
     </HStack>
   );
 };
