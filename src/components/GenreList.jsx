4af96fc277f5dfed4819463380d0662ecdc82ecb
madjo <78071957+madjo7@users.noreply.github.com>
2024-01-29 12:30:38 +0100

    Cleanup the genres

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -6,6 +6,7 @@ import {
   Image,
   Button,
   Spinner,
+  Heading,
 } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
@@ -16,33 +17,41 @@ const GenreList = ({ onSelectGenre, selectedGenre }) => {
   if (loading) return <Spinner></Spinner>;
 
   return (
-    <List>
-      {data.map((g) => (
-        <ListItem key={g.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize={"32px"}
-              borderRadius={8}
-              src={getCroppedImageUrl(g.image_background)}
-            ></Image>
-            <Button
-              fontWeight={
-                selectedGenre === null
-                  ? "normal"
-                  : g.id === selectedGenre.id
-                  ? "bold"
-                  : "normal"
-              }
-              onClick={() => onSelectGenre(g)}
-              variant="link"
-              fontSize={"lg"}
-            >
-              {g.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize={"2xl"} marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {data.map((g) => (
+          <ListItem key={g.id} paddingY="5px">
+            <HStack>
+              <Image
+                objectFit={"cover"}
+                boxSize={"32px"}
+                borderRadius={8}
+                src={getCroppedImageUrl(g.image_background)}
+              ></Image>
+              <Button
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={
+                  selectedGenre === null
+                    ? "normal"
+                    : g.id === selectedGenre.id
+                    ? "bold"
+                    : "normal"
+                }
+                onClick={() => onSelectGenre(g)}
+                variant="link"
+                fontSize={"lg"}
+              >
+                {g.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
